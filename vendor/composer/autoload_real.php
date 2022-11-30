c6a07a039edfb596981bf492537942f57371b24d
38672505883 <allan.shibata@gmail.com.br>
2022-11-30 11:24:33 -0300

    :package: Environment Variables Manager

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitbff5097f05c51f4f5f14bfcd6450e356
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitbff5097f05c51f4f5f14bfcd6450e356', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitbff5097f05c51f4f5f14bfcd6450e356', 'loadClassLoader'));
